5d913ba41878a5f545c63fcf573bb158a742620d
Abhilash4331 <abhilashkumar4331@gmail.com>
2024-03-28 17:41:37 +0530

    To do list using pinia

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    
+    <!--heading-->
+    <header>
+      <img src="./assets/logo/Pinialogo.svg.png" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <!-- new task form  -->
+    <div class="new-task-form">
+      <TaskForm  @add-task='onAddTask' />
+    </div>
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all' ">All tasks</button>
+      <button @click="filter = 'favs' ">Fav tasks</button>
+    </nav>
+
+    <!-- tasks list -->
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount() }} tasks left to do</p>
+      <div class="over" v-for="task in taskStore.tasks.tasks">
+        <taskDetails :task="task"/>
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div class="over" v-for="task in taskStore.favs">
+        <taskDetails :task="task"/>
+      </div>
+    </div>
+
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup>
+import { ref } from 'vue';
+import taskDetails from './components/taskDetails.vue'
+import { useTaskStore } from '../store/TaskStore';
+import TaskForm from './components/TaskForm.vue';
+const taskStore = useTaskStore()
+
+const filter = ref('all')
+</script>
+
+<style>
+@import './assets/logo/main.css';
+.over{
+  overflow-x: scroll;
 }
 </style>
